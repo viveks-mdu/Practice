3b1b3dc66e98796030369cc02298d560de8e9e61
Vivek Anand Sampath <visamp@microsoft.com>
2021-11-29 18:20:48 -0800

    add read config code

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace Practice
 {
@@ -8,7 +12,34 @@ namespace Practice
         {
             Console.WriteLine("Program started ...");
 
+            ReadConfigFile();
+
             Console.WriteLine("Program completed.");
+        }
+
+        private static void ReadConfigFile()
+        {
+            bool IsLocalSettingsRetrieved = true;
+            ReadOnlyDictionary<string, string> LocalConfigs;
+
+            string configFileName = $"Configuration.edog.json";
+            string configurationFile = Path.Combine("E:\\MediaTransformAndAnalysis\\container_build_artifacts\\docker_drop\\MeTAServiceWorkerRole\\config", configFileName);
+
+            try
+            {
+                string jsonConfig = File.ReadAllText(configurationFile);
+
+                Dictionary<string, string> localConfigs = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonConfig);
+                LocalConfigs = new ReadOnlyDictionary<string, string>(localConfigs);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"LocalSettings.TryInitialize() - Exception loading configurations from file {configurationFile}. Exception {ex}");
+                IsLocalSettingsRetrieved = false;
+                //throw;
+            }
+
+            Console.WriteLine(IsLocalSettingsRetrieved);
         }
     }
 }

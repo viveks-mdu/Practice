0ee091795f2f52f86d387408d85b476b5891e8c4
Vivek Anand Sampath <visamp@microsoft.com>
2021-11-29 18:02:42 -0800

    add sln

---
+++ Practice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program started ...");
+
+            Console.WriteLine("Program completed.");
+        }
+    }
+}
